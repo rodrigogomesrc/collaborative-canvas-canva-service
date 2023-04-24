26e4c7916ff7143246a8af8193c557e1d0fec354
Rodrigo Rocha <rocha.rodrigogomes@gmail.com>
2023-04-24 17:53:05 -0300

    cria projeto

---
+++ non-reactive/canva-service/src/main/java/br/ufrn/dimap/collaborativecanvas/canvaservice/CanvaServiceApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.dimap.collaborativecanvas.canvaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanvaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanvaServiceApplication.class, args);
+	}
+
+}
---
+++ non-reactive/canva-service/src/test/java/br/ufrn/dimap/collaborativecanvas/canvaservice/CanvaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.dimap.collaborativecanvas.canvaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanvaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
