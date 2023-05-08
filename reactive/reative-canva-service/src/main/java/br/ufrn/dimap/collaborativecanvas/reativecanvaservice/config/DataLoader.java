682c37ab7726fdb7855e23515f44d477ae62a33f
Rodrigo Rocha <rocha.rodrigogomes@gmail.com>
2023-05-08 09:13:07 -0300

    permite criar ou não a db baseado em configuração

--- reactive/reative-canva-service/src/main/java/br/ufrn/dimap/collaborativecanvas/reativecanvaservice/config/DataLoader.java
+++ reactive/reative-canva-service/src/main/java/br/ufrn/dimap/collaborativecanvas/reativecanvaservice/config/DataLoader.java
@@ -3,6 +3,7 @@ package br.ufrn.dimap.collaborativecanvas.reativecanvaservice.config;
 import br.ufrn.dimap.collaborativecanvas.reativecanvaservice.service.CanvasService;
 import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +21,12 @@ public class DataLoader implements ApplicationRunner {
 
     private final CanvasService canvasService;
 
+    @Value("${app.database.drop}")
+    private boolean dropTables = false;
+
+    @Value("${app.database.create}")
+    private boolean createTables = false;
+
     @Autowired
     public DataLoader(ConnectionFactory connectionFactory, CanvasService canvasService) {
         this.connectionFactory = connectionFactory;
@@ -28,16 +35,27 @@ public class DataLoader implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        String sql = new ClassPathResource("create-classes.sql").getFile().getAbsolutePath();
+
+        String sqlDrop = new ClassPathResource("drop-tables.sql").getFile().getAbsolutePath();
+        String contentDrop = Files.lines(Paths.get(sqlDrop)).collect(Collectors.joining("\n"));
+
+        String sql = new ClassPathResource("create-tables.sql").getFile().getAbsolutePath();
         String content = Files.lines(Paths.get(sql)).collect(Collectors.joining("\n"));
 
         DatabaseClient databaseClient = DatabaseClient.create(connectionFactory);
-        databaseClient.sql(content).fetch().rowsUpdated().block();
+        if (dropTables) {
+            System.out.println("Dropping tables");
+            databaseClient.sql(contentDrop).fetch().rowsUpdated().block();
+        }
 
-        System.out.println("Creating canvas");
-        canvasService.createCanvas("canvas1", 1L).subscribe();
-        canvasService.createCanvas("canvas2", 2L).subscribe();
-        canvasService.createCanvas("canvas3", 3L).subscribe();
+        if (createTables) {
+            System.out.println("Creating tables");
+            databaseClient.sql(content).fetch().rowsUpdated().block();
+            System.out.println("Creating canvas");
+            canvasService.createCanvas("canvas1", 1L).subscribe();
+            canvasService.createCanvas("canvas2", 2L).subscribe();
+            canvasService.createCanvas("canvas3", 3L).subscribe();
+        }
 
     }
 }
