7ada57caf78336fb19e45bd923e36e7ebd98d029
Rodrigo Rocha <rocha.rodrigogomes@gmail.com>
2023-05-14 12:00:14 -0300

    conserta configuração de virtual threads para mvc

--- non-reactive/canva-service/src/main/java/br/ufrn/dimap/collaborativecanvas/canvaservice/config/UseVirtualThreadCondition.java
+++ non-reactive/canva-service/src/main/java/br/ufrn/dimap/collaborativecanvas/canvaservice/config/UseVirtualThreadCondition.java
@@ -1,20 +1,18 @@
 package br.ufrn.dimap.collaborativecanvas.canvaservice.config;
 
 import org.jetbrains.annotations.NotNull;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 
+@Configuration
 public class UseVirtualThreadCondition implements Condition {
-
-
-    @Value("${app.use-virtual-threads}")
-    private boolean useVirtualThreads;
 
     @Override
     public boolean matches(@NotNull ConditionContext context, @NotNull AnnotatedTypeMetadata metadata) {
+        boolean useVirtualThreads = Boolean.TRUE.equals(context.getEnvironment().getProperty("app.use-virtual-threads", Boolean.class));
         System.out.println("=============== USING VIRTUAL THREADS: " + useVirtualThreads);
-        return useVirtualThreads;
+        return false;
     }
 }
