85b9760fae05445d0f3584bf32608cd02bcd6826
Rodrigo Rocha <rocha.rodrigogomes@gmail.com>
2023-06-27 17:56:39 -0300

    adiciona o import do redisson no mvc

---
+++ non-reactive/canva-service/src/main/java/br/ufrn/dimap/collaborativecanvas/canvaservice/config/CacheConfiguration.java
@@ -0,0 +1,20 @@
+package br.ufrn.dimap.collaborativecanvas.canvaservice.config;
+
+//import org.redisson.api.RedissonClient;
+//import org.redisson.spring.cache.RedissonSpringCacheManager;
+//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//import javax.cache.CacheManager;
+
+@Configuration
+public class CacheConfiguration {
+
+    /*
+    @Bean
+    public CacheManager cacheManager(RedissonClient redissonClient) {
+        return (CacheManager) new RedissonSpringCacheManager(redissonClient);
+    }
+
+     */
+}
